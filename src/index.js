23e18b51574fa4177b2d82a59ca514f6f01a0e15
rafael dias <dias416@gmail.com>
2022-06-04 18:23:39 +0100

    :tada: finished

--- src/index.js
+++ src/index.js
@@ -23,6 +23,18 @@ function verifyExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === "credit") {
+            return acc + operation.amount;
+        }else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post("/account", (request, response) => {//recurso de contas
     const { cpf, name } = request.body;
 
@@ -48,6 +60,85 @@ app.get("/statement", verifyExistsAccountCPF, (request, response) => {
     return response.json(customer.statement);
 });
 
+app.post("/deposit", verifyExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        create_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
+app.post("/withdraw", verifyExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount) {
+        return response.status(400).json({error: "Insufficient funds!"})
+    }
+
+    const statementOperation = {
+        amount,
+        create_at: new Date(),
+        type: "debit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
+app.get("/statement/date", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");//pega a data do dia independente do horario
+
+    const statement = customer.statement.filter((statement) => 
+        statement.create_at.toDateString() === new Date(dateFormat).toDateString()
+    );
+    return response.json(statement);
+});
+
+app.put("/account", verifyExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+});
+
+app.get("/account", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+})
+
+app.delete("/account", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    customers.splice( customer, 1 );
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+})
 app.listen(port, () => {
   console.log(`Server is running in port ${port}`)
 })
